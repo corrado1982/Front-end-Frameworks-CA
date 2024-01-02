a992e3749d8fbbd9709962a73c9e4bf0c327ee91
corrado1982 <104769882+corrado1982@users.noreply.github.com>
2024-01-02 23:51:14 +0100

    fixed search input

--- src/components/products/ProductList.jsx
+++ src/components/products/ProductList.jsx
@@ -6,22 +6,51 @@ import styles from "./ProductList.module.css";
 
 function ProductList() {
   const [posts, setPosts] = useState([]);
+  const [searchItem, setSearchItems] = useState("");
+  const [filteredItems, setFilteredItems] = useState([]);
+
   useEffect(() => {
     async function getData() {
       const response = await fetch(url);
       const json = await response.json();
       setPosts(json);
+      setFilteredItems(json);
       console.log(json);
     }
     getData();
   }, []);
+
+  const handleInputChange = (e) => {
+    const searchTerm = e.target.value;
+    setSearchItems(searchTerm);
+    console.log(searchTerm);
+
+    const filteredItem = posts.filter((post) =>
+      post.title.toLowerCase().startsWith(searchTerm.trim().toLowerCase())
+    );
+
+    setFilteredItems(filteredItem);
+  };
   //provo a levare un div
   return (
-    <div className={styles.productcontainer}>
-      {posts.map((post) => {
-        console.log(post);
-        return <SingleProduct key={post.id} post={post} />;
-      })}
+    <div>
+      <input
+        type="text"
+        value={searchItem}
+        onChange={handleInputChange}
+        placeholder="Type to search"
+      />
+      {/* <ul>
+        {filteredItems.map((item) => (
+          <li key={item.id}>{item.title}</li>
+        ))}
+      </ul> */}
+      <div className={styles.productcontainer}>
+        {filteredItems.map((item) => {
+          console.log(item);
+          return <SingleProduct key={item.id} post={item} />;
+        })}
+      </div>
     </div>
   );
 }
