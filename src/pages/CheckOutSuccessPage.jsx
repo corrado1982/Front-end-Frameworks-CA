5b75fa91f401c7d6ff77eac4eb731e2f406cee53
corrado1982 <104769882+corrado1982@users.noreply.github.com>
2024-01-07 18:57:51 +0100

    added refresh function when goes to homepage CheckOutSuccessPage.jsx

--- src/pages/CheckOutSuccessPage.jsx
+++ src/pages/CheckOutSuccessPage.jsx
@@ -2,6 +2,10 @@ import { Link } from "react-router-dom";
 import Button from "../styles/Button";
 import styles from "./CheckOutSuccessPage.module.css";
 
+function refreshPage() {
+  window.location.reload();
+}
+
 function CheckOutSuccesPage() {
   return (
     <div className={styles.successFeedback}>
@@ -9,7 +13,7 @@ function CheckOutSuccesPage() {
         <h1>Congratulations</h1>
         <h2>Your order will be delivered as soon as possible</h2>
       </div>
-      <Button>
+      <Button onClick={refreshPage}>
         <Link to={"/"}>Back to shop</Link>
       </Button>
     </div>
